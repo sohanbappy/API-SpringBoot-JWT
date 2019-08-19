bd6b74e088b069c13fae6da97e52701d49745406
sohanbappy <sohanbappy@gmail.com>
2019-08-20 01:38:10 +0600

    project created

---
+++ src/main/java/com/rokoassessment/RokoAssesmentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rokoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokoAssesmentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokoAssesmentDemoApplication.class, args);
+	}
+
+}
