8dc3c35e98c39516b4f0ba839721441cf89aa48c
sohanbappy <sohanbappy@gmail.com>
2019-08-21 11:07:47 +0600

    services added

---
+++ src/main/java/com/rokoassessment/services/DoctorService.java
@@ -0,0 +1,12 @@
+package com.rokoassessment.services;
+
+import org.springframework.stereotype.Service;
+import com.rokoassessment.models.Doctor;
+
+@Service
+public class DoctorService {
+
+	public boolean isValid(Doctor doc) {
+		return true;
+	}
+}
---
+++ src/main/java/com/rokoassessment/services/PatientService.java
@@ -0,0 +1,12 @@
+package com.rokoassessment.services;
+
+import org.springframework.stereotype.Service;
+import com.rokoassessment.models.Patient;
+
+@Service
+public class PatientService {
+
+	public boolean isValid(Patient doc) {
+		return true;
+	}
+}
