58c3e4a9b291537de2bbee711ecb7b9cf3cfdf73
sohanbappy <sohanbappy@gmail.com>
2019-08-21 18:09:44 +0600

    validation for patient added

--- src/main/java/com/rokoassessment/controllers/PatientController.java
+++ src/main/java/com/rokoassessment/controllers/PatientController.java
@@ -3,6 +3,8 @@ package com.rokoassessment.controllers;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,30 +15,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.rokoassessment.dao.PatientRepo;
 import com.rokoassessment.models.Patient;
+import com.rokoassessment.services.PatientService;
 
 @RestController
 public class PatientController {
 	
 	@Autowired
 	private PatientRepo patrepo;
+	private PatientService patServ; 
 
 	@GetMapping(path="/api/patients")
 	public List<Patient> getAllPatients(){
 		return patrepo.findAll();
 	}
 	@PostMapping(path="/api/insert/patient/new", consumes = {"application/json"})
-	public Patient addPatient(@RequestBody Patient pat) {
+	public ResponseEntity<String> addPatient(@RequestBody Patient pat) {
+		//validate
+		boolean valid = patServ.isValid(pat);
+		if(valid) {
 		patrepo.save(pat);
-		return pat;
+		return new ResponseEntity<String>("created",HttpStatus.OK);
+		}else {
+		return new ResponseEntity<String>("not created",HttpStatus.BAD_REQUEST);
+		}
 	}
 	@DeleteMapping("/api/delete/patients/{id}")
-	public String deletePatient(@PathVariable(value="id") int pat_id){
+	public ResponseEntity<String> deletePatient(@PathVariable(value="id") int pat_id){
 		Patient patient = patrepo.getOne(pat_id);
 		patrepo.delete(patient);
-		return "deleted";
+		return new ResponseEntity<String>("deleted",HttpStatus.OK);
 	}
 	@PutMapping(path="/api/update/patients/{id}", consumes = {"application/json"})
-	public String saveOrUpdatePatient(@RequestBody Patient pat,@PathVariable(value="id") int pat_id){
+	public ResponseEntity<String> saveOrUpdatePatient(@RequestBody Patient pat,@PathVariable(value="id") int pat_id){
 		Patient patient = patrepo.getOne(pat_id);
 		patient.setName(pat.getName());
 		patient.setAge(pat.getAge());
@@ -44,9 +54,15 @@ public class PatientController {
 		patient.setMobile(pat.getMobile());
 		patient.setOccupation(pat.getOccupation());
 		patient.setSymptom_summary(pat.getSymptom_summary());
+		//validate
+		boolean valid = patServ.isValid(pat);
+		if(valid) {
 		patrepo.save(patient);
+		return new ResponseEntity<String>("updated",HttpStatus.OK);
+		}else {
+			return new ResponseEntity<String>("not updated",HttpStatus.BAD_REQUEST);	
+		}
 		
-		return "updated";
 	}
 	@RequestMapping(path="api/patients/{id}")
 	public Optional<Patient> getPatient(@PathVariable(value="id") int pat_id){
--- src/main/java/com/rokoassessment/services/PatientService.java
+++ src/main/java/com/rokoassessment/services/PatientService.java
@@ -6,7 +6,15 @@ import com.rokoassessment.models.Patient;
 @Service
 public class PatientService {
 
-	public boolean isValid(Patient doc) {
-		return true;
+	public boolean isValid(Patient pat) {
+		if(pat.getName()==null || pat.getName().isEmpty() || pat.getMobile()==null || pat.getMobile().isEmpty() || pat.getAge()<=0 ||
+				pat.getGender()==null || pat.getGender().isEmpty() || pat.getOccupation()==null || pat.getOccupation().isEmpty()) {
+			return false;
+		}
+		if(pat.getName().matches("^(([a-z]+)[ ]?)*$")) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
